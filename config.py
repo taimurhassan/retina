b5e8d879404c7f91a842587847a4b0349ccb8600
Taimur Hassan <70101972+taimurhassan@users.noreply.github.com>
2022-10-26 23:12:48 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Note: please modify these parameters as per your application need. 
+# number of datasets 
+K = 7
+folderPath = "testingDataset/test_images/"
+resultsPath = "testingDataset/segmentation_results"
+newClasses = [6, 4, 6] 
+imgHeight = 576
+imgWidth = 768
